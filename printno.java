b250acabcc4f2b0ad061fa8e4baf3352c95d418c
madhavgit <sanjay.madhav123@gmail.com>
2021-12-05 17:43:37 +0530

    print no java programm added

---
+++ printno.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class printno{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        print(n);
+    }
+    
+    public static void print(int n){
+        if( n == 0){
+            System.out.println("1");
+            return;
+        }
+        print(n-1);
+        System.out.println(n);
+    }
+    
+}
